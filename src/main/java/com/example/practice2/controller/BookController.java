9ce40e27f67c496be93198a8e19368746c0130d5
geon <zqrc05@gmail.com>
2024-08-20 18:42:17 +0900

    도서 추가 형식만 만들어봄!

--- src/main/java/com/example/practice2/controller/BookController.java
+++ src/main/java/com/example/practice2/controller/BookController.java
@@ -13,6 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -37,6 +40,13 @@ public class BookController {
     @GetMapping("/book/bookWrite")
     public void bookWrite() {
     }
+
+    @PostMapping("/book/bookWriteAction")
+    public String postMethodName() {
+        
+        return "";
+    }
+    
     
     @GetMapping("/book/bookDelete")
     public String bookDelete(@RequestParam(required = false, name = "book_no") String no, Model model) {
